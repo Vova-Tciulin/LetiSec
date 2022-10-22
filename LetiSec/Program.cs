2c1a173b3bc4b93c88976be687a7584a55d03147
VT <vtciulin@mail.ru>
2022-10-22 22:38:53 +0300

    add packet for react

--- LetiSec/Program.cs
+++ LetiSec/Program.cs
@@ -1,9 +1,20 @@
 using LetiSec.Models.DbModel;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+  .AddV8();
 
 builder.Services.AddControllersWithViews();
 //получаем строку подключения сервера бд
@@ -25,6 +36,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+
+app.UseReact(config =>config.AddScript("~/js/First.jsx")
+                        .AddScript("~/js/Second.jsx")
+);
+
 app.UseStaticFiles();
 
 app.UseRouting();
