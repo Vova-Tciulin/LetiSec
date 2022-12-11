c5d79f7199c0280207a34c406f75a20037bda62c
VT <vtciulin@mail.ru>
2022-12-11 16:54:36 +0300

    Добавлена тех. Поддержка

--- LetiSec/Models/ViewModels/ChatBoxVM.cs
+++ LetiSec/Models/ViewModels/ChatBoxVM.cs
@@ -6,6 +6,7 @@ namespace LetiSec.Models.ViewModels
 	{
 		public SuppMessage SuppMessage { get; set; }
 		public List<AllMessage> Msg { get; set; } = new List<AllMessage>();
+		public bool IsUser { get; set; }
 	}
 
 	public class AllMessage
